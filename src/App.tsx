be30371b0d5431e28f73c004cdc567eca42e47c5
Wadia Jara Quesieh <wjaraquesieh@academic.rrc.ca>
2024-10-05 20:04:03 -0500

    Show some components in the App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button/Button'
+import Card from './Card/Card'
+import Table, { TableHeader, TableRow, TableCell, TableFooter } from './Table';
+import RadioButton from './RadioButton';
+import Label from './Label';
+import Dropdown from './Dropdown';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>UI Component Library</h1>
+
+      <h2>Button Component</h2>
+      <Button children={"This is a Button"} disabled={false} onClick={() => alert('Primary Button Clicked')} />
+      <Button children={"This is a Button"} disabled={true} onClick={() => alert('Secondary Button Clicked')} />
+
+      <h2>Card Component</h2>
+      <Card children={"Card"} />
+
+      <h2>Table</h2>
+      <Table children={
+        <>
+          <TableHeader children={
+            <>
+              <TableCell children={"Column 1"} />
+              <TableCell children={"Column 2"} />
+              <TableCell children={"Column 3"} />
+            </>
+          } />
+          <tbody>
+            <TableRow children={
+              <>
+                <TableCell children={"Row 1"} />
+                <TableCell children={"Row 2"} />
+                <TableCell children={"Row 3"} />
+              </>
+            } />
+          </tbody>
+          <TableFooter children={"Footer"} />
+        </>
+      } />
+
+      <h2>Radio Button</h2>
+      <Label htmlFor='rbOption' children={"Select an Option"} />
+      <RadioButton label='Option 1' checked={false} name='rbOption' value='1'  />
+      <RadioButton label='Option 2' checked={true} name='rbOption' value='2'  />
+
+      <h2>Dropdown</h2>
+      {/* <Dropdown options={} /> */}
+
+      <h2>Image</h2>
+
     </div>
   );
 }
